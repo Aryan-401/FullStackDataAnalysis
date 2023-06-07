256fa10f449d2d498c8355daef1b4a4786ef6591
Aryan-401 <aryan.raj.garg@gmail.com>
2023-06-07 22:54:15 +0530

    refactored

--- data/scripts/geospacial_data.py
+++ data/scripts/geospacial_data.py
@@ -7,9 +7,5 @@ data['centroid'] = data['geometry'].centroid
 data['coordinate_x'] = data['centroid'].apply(lambda p: p.x)
 data['coordinate_y'] = data['centroid'].apply(lambda p: p.y)
 
-data = data[['location_id', 'zone', 'coordinate_x', 'coordinate_y']]
-none_values1 = pd.DataFrame({'location_id': 264, 'zone': 'N.A', 'coordinate_x': 0, 'coordinate_y': 0}, index=[263])
-none_values2 = pd.DataFrame({'location_id': 265, 'zone': 'N.A', 'coordinate_x': 0, 'coordinate_y': 0}, index=[264])
-data = pd.concat([data, none_values1, none_values2])
-data.to_csv('../taxi_zones.csv', index=False)
+data[['location_id', 'zone', 'coordinate_x', 'coordinate_y']].to_csv('../taxi_zones.csv', index=False)
 
