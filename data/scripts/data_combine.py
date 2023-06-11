9d92fc47f236464ff3eb306fd603d373ed9afdb6
Aryan-401 <aryan.raj.garg@gmail.com>
2023-06-11 23:19:59 +0530

    uncomment

--- data/scripts/data_combine.py
+++ data/scripts/data_combine.py
@@ -10,14 +10,14 @@ for i in range(1, 13):
 
     df = pd.read_parquet(f'../yellow_tripdata_2022-{month}.parquet')
     ___df___ = df.sample(n=8500)
-    # _df_ = pd.concat([_df_, df])
+    _df_ = pd.concat([_df_, df])
     __df__ = pd.concat([__df__, ___df___])
     print(f'{_df_.shape}, {i}/12')
 
 print('Beginning to save...')
 __df__.to_csv('../yellow_tripdata_2022_sample.csv', index=False)
 print('Sample saved!')
-# _df_.to_csv('../yellow_tripdata_2022.csv', index=False)
+_df_.to_csv('../yellow_tripdata_2022.csv', index=False)
 print('All data saved!')
 
 
